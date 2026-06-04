1cb12af091f246f3d3109323c3c260bf3953e201
miguel silva <miguelsil699@gmail.com>
2026-06-04 10:31:20 +0100

    Finaly, first commit for the new version of the System

---
+++ demo/src/main/java/com/example/App.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ * 
+ * @author Miguel Oliveira
+ * @version Alpha 0.0.1
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("primary.fxml"));
+        scene = new Scene(fxmlLoader.load(), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
