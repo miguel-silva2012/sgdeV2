d622636d02f4bb8598c35914bb6dd3313fd93ce0
miguel silva <miguelsil699@gmail.com>
2026-06-06 12:58:56 +0100

    Correction of some bugs

--- demo/src/main/java/com/example/SQLDataBaseConection.java
+++ demo/src/main/java/com/example/SQLDataBaseConection.java
@@ -2,25 +2,43 @@ package com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SQLDataBaseConection {
     private static final String user = "root",
-            password = "root",
-            URL = "jdbc:mysql://localhost:3306/cadastro";
+            password = "",
+            URL = "jdbc:mysql://localhost:3306/clothes";
 
     private static Connection conn;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        System.out.println(getColumn("id"));
+        System.out.println(getColumn("price"));
+        System.out.println(getColumn("name"));
+    }
+
+    public static List<String> getColumn(String column) throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-
-            if (conn == null) {
-                conn = DriverManager.getConnection(URL, user, password);
-            }
-
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
         }
+
+        List<String> columnOfTable = new ArrayList<String>();
+
+        if (conn == null) {
+            conn = DriverManager.getConnection(URL, user, password);
+
+            ResultSet rsClient = conn.createStatement().executeQuery("SELECT * FROM tableclothes");
+
+            while (rsClient.next()) {
+                columnOfTable.add(rsClient.getString(column));
+            }
+        } 
+        
+        return columnOfTable;
     }
 }
