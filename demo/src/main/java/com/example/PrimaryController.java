d8ebb5bcbbe22d3f23e3fbd1e0c804d14cce28f2
miguel silva <miguelsil699@gmail.com>
2026-06-11 19:50:53 +0100

    Some details

--- demo/src/main/java/com/example/PrimaryController.java
+++ demo/src/main/java/com/example/PrimaryController.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 
 public class PrimaryController {
     @FXML
