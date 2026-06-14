55df3a6b7611ec795252d65c804748f15813ff8a
miguel silva <miguelsil699@gmail.com>
2026-06-14 21:29:49 +0100

    Especifications

--- demo/src/main/java/com/example/Controllers/PrimaryController.java
+++ demo/src/main/java/com/example/Controllers/PrimaryController.java
@@ -11,15 +11,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 public class PrimaryController {
     @FXML
